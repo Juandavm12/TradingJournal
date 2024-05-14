f798f6e9404b8049b32001898785a74ec5b8cafb
edwin <edwin@DESKTOP-CCMSVUV>
2024-05-14 11:16:28 -0500

    user

--- TradingJournal/TradingJournal.Web/Program.cs
+++ TradingJournal/TradingJournal.Web/Program.cs
@@ -1,19 +1,22 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using TradingJournal.Web;
+using TradingJournal.Web.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
-builder.Services.AddSweetAlert2(options => {
-    options.Theme = SweetAlertTheme.Borderless;
-});
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 await builder.Build().RunAsync();
