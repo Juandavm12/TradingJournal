33fbfdb62e69c0f4edfc476f649b6f84c99c70c5
edwin <edwin@DESKTOP-CCMSVUV>
2024-05-27 01:24:19 -0500

    Top 200 coins

--- TradingJournal/TradingJournal.Shared/Entities/Coin.cs
+++ TradingJournal/TradingJournal.Shared/Entities/Coin.cs
@@ -11,7 +11,6 @@ namespace TradingJournal.Shared.Entities
         public decimal Current_price { get; set; }
         public decimal Market_cap { get; set; }
         public long Market_cap_rank { get; set; }
-        public decimal fully_diluted_valuation { get; set; }
         public decimal Total_volume { get; set; }
         public decimal High_24h { get; set; }
         public decimal Low_24h { get; set; }
@@ -20,8 +19,7 @@ namespace TradingJournal.Shared.Entities
         public decimal market_cap_change_24h { get; set; }
         public decimal market_cap_change_percentage_24h { get; set; }
         public decimal circulating_supply { get; set; }
-        public decimal total_supply { get; set; }
-        public decimal ath { get; set; }
+         public decimal ath { get; set; }
         public decimal atl_change_percentage { get; set; }
         public string atl_date { get; set; }
         public string last_updated { get; set; }
