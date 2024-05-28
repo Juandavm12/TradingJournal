c7b901c0468f87f7b1ceacefb4d2ae42d0344432
edwin <edwin@DESKTOP-CCMSVUV>
2024-05-28 13:13:16 -0500

    Azure Publish

--- TradingJournal/TradingJournal.API/Program.cs
+++ TradingJournal/TradingJournal.API/Program.cs
@@ -5,7 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.Text;
 using TradingJournal.API.Data;
 using TradingJournal.API.Helpers;
@@ -20,9 +22,40 @@ builder.Services.AddEndpointsApiExplorer();
 
 
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Trading Journal API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. <br /> <br />
+ Enter 'Bearer' [space] and then your token in the text input below.<br /> <br />
+ Example: 'Bearer 12345abcdef'<br /> <br />",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+ {
+ {
+ new OpenApiSecurityScheme
+ {
+ Reference = new OpenApiReference
+ {
+ Type = ReferenceType.SecurityScheme,
+ Id = "Bearer"
+ },
+ Scheme = "oauth2",
+ Name = "Bearer",
+ In = ParameterLocation.Header,
+ },
+ new List<string>()
+ }
+ });
+});
 
 
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name = WindowsSecurity"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name = Connectionwhitparams"));
 
 
 builder.Services.AddIdentity<User, IdentityRole>(x =>
@@ -81,11 +114,15 @@ static void SeedData(WebApplication app)
     }
 }
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
-    app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
